a5e358cd9da62487bbef2cdb5d662c4ad44f0692
Sean Liao <seankhliao@gmail.com>
2021-02-07 11:44:31 +0100

    null_resource

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "kind" {
+resource "null_resource" "cluster26" {
   triggers = {
     kind_config = file("kind.k8s.yaml")
   }
@@ -9,5 +9,10 @@ resource "null_resource" "kind" {
 
   provisioner "local-exec" {
     command = "kind create cluster --config kind.k8s.yaml --name cluster26"
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kind delete cluster --name cluster26"
   }
 }
