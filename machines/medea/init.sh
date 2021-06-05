a7ab0b426db886e4480db339c9ccc6361dcd9b85
Sean Liao <seankhliao@gmail.com>
2021-06-05 12:06:34 +0200

    wip

--- machines/medea/init.sh
+++ machines/medea/init.sh
@@ -11,7 +11,7 @@ FILES=(
     '/root/.ssh/authorized_keys'
     '/etc/sysctl.d/30-ipforward.conf'
     '/etc/modules-load.d/br_netfilter.conf'
-    '/etc/kubernetes/kubeadm.yaml'
+    # '/etc/kubernetes/kubeadm.yaml'
     '/etc/systemd/network/40-wg0.netdev'
     '/etc/systemd/network/41-wg0.network'
 )
@@ -20,10 +20,10 @@ for f in ${FILES[@]} ; do
 done
 
 ssh ${HOST} pacman -Rns --noconfirm btrfs-progs gptfdisk haveged xfsprogs wget vim net-tools cronie || true
-ssh ${HOST} pacman -Syu --noconfirm neovim containerd kubeadm kubelet open-iscsi nfs-utils
-ssh ${HOST} systemctl enable containerd kubelet iscsid
-ssh ${HOST} sh -c "'rm /etc/kubernetes/kubelet.env'" || true
-ssh ${HOST} reboot || true
+# ssh ${HOST} pacman -Syu --noconfirm neovim containerd kubeadm kubelet open-iscsi nfs-utils
+# ssh ${HOST} systemctl enable containerd kubelet iscsid
+# ssh ${HOST} sh -c "'rm /etc/kubernetes/kubelet.env'" || true
+# ssh ${HOST} reboot || true
 
 # ssh ${HOST} kubeadm init --skip-phases=mark-control-plane,addon  --config /etc/kubernetes/kubeadm.yaml
 # rsync --progress medea:/etc/kubernetes/admin.conf ~/.config/kube/config
